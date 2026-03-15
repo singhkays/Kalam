543c4601c35211a021aa49b26e2fb7255257bbfb
Kay Singh <singhkays@users.noreply.github.com>
2026-03-15 00:38:55 -0700

    Trigger landing page deploy workflow

--- landing-page/vite.config.ts
+++ landing-page/vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [
     // The React and Tailwind plugins are both required for Make, even if
     // Tailwind is not being actively used – do not remove them
+    // Trigger workflow update
     react(),
     tailwindcss(),
   ],
